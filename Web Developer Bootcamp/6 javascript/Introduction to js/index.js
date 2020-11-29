2d4892668bb43acf791ae70be7d41b34a7eed14b
DNg <dheerajnagpal21081999@yahoo.com>
2020-11-29 11:48:27 +0530

    alert prompt and console.log

---
+++ Web Developer Bootcamp/6 javascript/Introduction to js/index.js
@@ -0,0 +1,7 @@
+var userFName = prompt("Your First Name Human!");
+var userLName = prompt("Your Last Name Human!");
+console.log("Nice to Meet You " + userFName + " " + userLName);
+alert("Nice to Meet You " + userFName + " " + userLName);
+
+var age = prompt("Tell Me ur Age Human!");
+alert("Nice to Meet You " + age + " age Human");
