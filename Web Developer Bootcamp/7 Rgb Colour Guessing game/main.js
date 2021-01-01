1e0d76bc66ca963c000825438fe8bed61cd5e22a
DNg <dheerajnagpal21081999@yahoo.com>
2021-01-01 14:07:41 +0530

    removing hard code

--- Web Developer Bootcamp/7 Rgb Colour Guessing game/main.js
+++ Web Developer Bootcamp/7 Rgb Colour Guessing game/main.js
@@ -8,8 +8,10 @@ var pickedColor;
 var nSquares = 6;
 var pickedColorDisp = document.getElementById("pickedColor");
 var newColors = document.querySelector("#reset");
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.querySelector("#hardBtn");
+// var easyBtn = document.querySelector("#easyBtn");
+// var hardBtn = document.querySelector("#hardBtn");
+
+var modes = document.querySelectorAll(".mode");
 
 function hitSquare() {
     squares[i].addEventListener("click", function() {
@@ -38,20 +40,27 @@ function changeColors() {
     }
 }
 
-    function reset() {
-        nSquares = 6;
-        if(easyBtn.classList.length > 0) {
-            nSquares = 3;
-        }
-        colors = giveColorRandom(nSquares);
-        pickedColor = pickColorRandom(nSquares);
-        pickedColorDisp.textContent = pickedColor;
-        for(var i=0; i<nSquares; i++) {
+function reset() {
+    // nSquares = 6;
+    // if(easyBtn.classList.length > 0) {
+    //     nSquares = 3;
+    // }
+    colors = giveColorRandom(nSquares);
+    pickedColor = pickColorRandom(nSquares);
+    pickedColorDisp.textContent = pickedColor;
+    for(var i=0; i<squares.length; i++) {
+        if(colors[i]){  
+            squares[i].style.display = "block";     
             squares[i].style.backgroundColor = colors[i];
         }
-        h1.style.background = "rgb(61, 17, 61)";
-        messageDisp.textContent = "";
-        newColors.textContent = "New Colors";
+        else {
+            squares[i].style.display = "none";
+        } 
+
+    }
+    h1.style.background = "rgb(61, 17, 61)";
+    messageDisp.textContent = "";
+    newColors.textContent = "New Colors";
 }
 
 function pickColorRandom(number) {
@@ -102,28 +111,64 @@ pickedColorDisp.textContent = pickedColor;
 // Button New Colors!
 
 newColors.addEventListener("click", function() {
-    reset();
+    reset(nSquares);
 });
 
 
-easyBtn.addEventListener("click", function() {
-    this.classList.add("selected");
-    hardBtn.classList.remove("selected");
 
-    colors = giveColorRandom(3);
-    pickedColor = pickColorRandom(3);
-    pickedColorDisp.textContent = pickedColor;
-    reset();
-    for(var i=3; i<squares.length; i++){
-        squares[i].style.display = "none";
-    }
-});
+for(var i=0; i<modes.length; i++) {
 
-hardBtn.addEventListener("click", function() {
-    this.classList.add("selected");
-    easyBtn.classList.remove("selected");
-    reset();
-    for(var i=3; i<squares.length; i++){
-        squares[i].style.display = "block";
-    }
-});
+    // *************************** which mode to be selected *******************************
+    //******************** mode[0] ==== easy .. mode[1] ==== hard ************************
+    modes[i].addEventListener("click", function() {
+        modes[0].classList.remove("selected"); // if this\clicked button is hard ie mode[1];
+        modes[1].classList.remove("selected"); // if this->refers to easy ie mode[0];
+        this.classList.add("selected");
+
+// ***************************************************************************************
+        // figure out how many squares to show
+        // pick new colors
+        // pick new pickedColor
+        // update page to refelect changes
+        console.log(modes[0].classList.length);
+        if(this.textContent == "Easy") {
+            nSquares = 3;
+            
+            // for(var i=3; i<squares.length; i++){
+            //     squares[i].style.display = "none";
+            // }
+        } else {
+            nSquares = 6;
+            // for(var i=3; i<squares.length; i++){
+            //     squares[i].style.display = "block";
+            // }
+        }
+        reset(nSquares);
+    });
+
+}
+// ******************************************************************************************
+// ************************************* Hard Code of easy button and Hard button *********************
+
+// easyBtn.addEventListener("click", function() {
+//     this.classList.add("selected");
+//     hardBtn.classList.remove("selected");
+
+//     colors = giveColorRandom(3);
+//     pickedColor = pickColorRandom(3);
+//     pickedColorDisp.textContent = pickedColor;
+//     reset();
+    // for(var i=3; i<squares.length; i++){
+    //     squares[i].style.display = "none";
+    // }
+//     
+// });
+
+// hardBtn.addEventListener("click", function() {
+//     this.classList.add("selected");
+//     easyBtn.classList.remove("selected");
+//     reset();
+//     for(var i=3; i<squares.length; i++){
+//         squares[i].style.display = "block";
+//     }
+// });
