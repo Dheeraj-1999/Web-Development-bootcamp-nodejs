f63202254830703e514f3b37b2f71343ae755774
DNg <dheerajnagpal21081999@yahoo.com>
2021-01-06 13:13:40 +0530

    jQuery selection and manipulation basics

---
+++ Web Developer Bootcamp/8 jQuery/3 exercise/jQuery.js
@@ -0,0 +1,5 @@
+// alert("connected!");
+$("div").css("backgroundColor", "purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border", "2px solid orange");
+$("div:first-child").css("color", "pink");
