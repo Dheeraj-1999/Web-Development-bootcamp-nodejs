d16ed16d5fa28d801496cc1d3177a4b02cf3ca83
DNg <dheerajnagpal21081999@yahoo.com>
2021-01-09 14:49:42 +0530

    // creation of new toDo by entering in input box

--- Web Developer Bootcamp/9 To Do list project/assets/js/todos.js
+++ Web Developer Bootcamp/9 To Do list project/assets/js/todos.js
@@ -1,14 +1,34 @@
-// alert("Connected!")
 
-$("li").on("click", function() {
+// when clicked on li 
+$("ul").on("click", "li", function() {
+    // this will give lineThrough effect
     $(this).toggleClass("clickedli");
     console.log("clickedLi!");
 });
 
-$(".clicked").on("click", function(event) {
+
+// When Clicked on x remove that list item after some fadeout 
+$("ul").on("click", ".clicked", function(event) {
     $(this).parent().fadeOut(function() {
+        /*after fadeout this function will be invoked*/
         $(this).remove();
     })
     console.log("removedLi!");
+    // This will be invoked to stop bubble up!
     event.stopPropagation();
+});
+
+
+// creation of new toDo by entering in input box;
+$("input[type='text']").on("keypress", function(event) {
+    if(event.which === 13 /*if user hit enter*/) {
+        // grabbing new todo text
+        var todoText = ($(this).val());
+        
+        // on enetring input must go away;
+        $(this).val("");
+        // appendind the li html code!
+        var liText = "<li><span class='clicked'>X</span> " + todoText + "</li>"
+        $("ul").append(liText);
+    }
 });
