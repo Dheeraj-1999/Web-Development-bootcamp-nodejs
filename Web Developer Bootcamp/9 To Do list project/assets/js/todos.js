77e1865ce74ba126ec4926345caffba4c09cc060
DNg <dheerajnagpal21081999@yahoo.com>
2021-01-09 13:48:41 +0530

    click on x the remove the li

--- Web Developer Bootcamp/9 To Do list project/assets/js/todos.js
+++ Web Developer Bootcamp/9 To Do list project/assets/js/todos.js
@@ -4,3 +4,11 @@ $("li").on("click", function() {
     $(this).toggleClass("clickedli");
     console.log("clickedLi!");
 });
+
+$(".clicked").on("click", function(event) {
+    $(this).parent().fadeOut(function() {
+        $(this).remove();
+    })
+    console.log("removedLi!");
+    event.stopPropagation();
+});
