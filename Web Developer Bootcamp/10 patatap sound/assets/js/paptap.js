78808f66210d430e97a4895b06da2c0806ab2066
DNg <dheerajnagpal21081999@yahoo.com>
2021-01-11 23:22:56 +0530

    random circles on key press

--- Web Developer Bootcamp/10 patatap sound/assets/js/paptap.js
+++ Web Developer Bootcamp/10 patatap sound/assets/js/paptap.js
@@ -1,27 +1,35 @@
-var CrBwDist = 100
-for(var i=0; i<1000; i+=100) { // i down circles 
-    for(var j=0; j<1000; j+=100) { // j right side circles
-        // var myCircle = new Path.Circle(new Point((j*CrBwDist)+(10*j), (i*CrBwDist)+(10*i)), 10);
-        var myCircle = new Path.Circle(new Point(i, j), 10);
-        myCircle.fillColor = 'white'   
-    }
+// var CrBwDist = 100
+// for(var i=0; i<1000; i+=100) { // i down circles 
+//     for(var j=0; j<1000; j+=100) { // j right side circles
+//         // var myCircle = new Path.Circle(new Point((j*CrBwDist)+(10*j), (i*CrBwDist)+(10*i)), 10);
+//         
+//         // myCircle.fillColor = 'white'   
+//     }
+
+
+
+// Create a centered text item at the center of the view:
+// var text = new PointText({
+// 	point: view.center,
+// 	content: 'Click here to focus and then press some keys.',
+// 	justification: 'center',
+// 	fontSize: 15
+// });
+
+function onKeyDown(event) {
+	// When a key is pressed, set the content of the text item:
+    // text.content = 'The ' + event.key + ' key was pressed!';
+    var r = Math.floor(Math.random()*255);
+    var g = Math.floor(Math.random()*255);
+    var b = Math.floor(Math.random()*255);
+    // console.log(r, g, b);
+    var circle = new Path.Circle(new Point(Math.random()*1373, Math.random()*674), Math.random()*50);
+    circle.fillColor = "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
-// for(var i=0; i<1000; i++) {
-//     var myCircle = new Path.Ca'2114cjlircle(new Point(Math.random()*2000, Math.random()*1000), 5);
-//     var r = Math.random()*255;
-//     var g = Math.random()*255;
-//     var b = Math.random()*255;
-
-//     myCircle.fillColor = "rgb(2*i+(5*i), 200+(i*10), 0)";
-//     Math.random()
+// function onKeyUp(event) {
+//     // When a key is released, set the content of the text item:
+//     new Path.Circle(new Point(50, 50), 51).fillColor = "black";
+//     new Path.Circle(new Point(50, 50), 2).fillColor = "white";
+//     new Path.Circle(new Point(50, 50), 2).fillColor = "blacK";
 // }
-
-// var myCircle = new Path.Circle(new Point(15, 30), 10);
-// myCircle.fillColor = 'white';
-// var myCircle = new Path.Circle(new Point(15, 30+20+10), 10);
-// myCircle.fillColor = 'white';
-// var myCircle = new Path.Circle(new Point(15, 30+40+20), 10);
-// myCircle.fillColor = 'white';
-// var myCircle = new Path.Circle(new Point(15, 30+60+30), 10);
-// myCircle.fillColor = 'white';
